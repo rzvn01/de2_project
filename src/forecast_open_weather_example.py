5fd16e81a49028646c7731a684e3e0ae19abda8e
raszvan <r_cacu@yahoo.com>
2023-11-24 21:57:04 +0100

    saved progress

--- src/forecast_open_weather_example.py
+++ src/forecast_open_weather_example.py
@@ -4,111 +4,34 @@ from i2c_lcd import I2cLcd
 from time import sleep
 from ldr import LDR
 from water_sensor import WaterSensor
+from wifi_manager import WiFiManager
+from lcd_oled import SH1106_I2C
+from machine import soft_reset
 import bme280
 import urequests
 import json
 import network
+from display_manager import DisplayManager
 
 
-class WiFiManager:
-    def __init__(self, ssid, password):
-        self.ssid = ssid
-        self.password = password
-        self.wifi = network.WLAN(network.STA_IF)
-        
-    def connect_wifi(self):
-        """
-        Connect to Wi-Fi network.
-
-        Activates the Wi-Fi interface, connects to the specified network,
-        and waits until the connection is established.
-
-        :return: None
-        """
-        from time import sleep_ms
-
-        if not self.wifi.isconnected():
-            # Activate the Wi-Fi interface
-            self.wifi.active(True)
-
-            # Connect to the specified Wi-Fi network
-            self.wifi.connect(self.ssid, self.password)
-
-            # Wait until the connection is established
-            print(f"Connecting to {self.ssid}", end="")
-            while not self.wifi.isconnected():
-                print(".", end="")
-                sleep(1)
-
-            print(" Done")
-        else:
-            print("Already connected")
-
-
-    def disconnect_wifi(self):
-        """
-        Disconnect from Wi-Fi network.
-
-        Deactivates the Wi-Fi interface if active and checks if
-        the device is not connected to any Wi-Fi network.
-
-        :return: None
-        """
-        # Check if the Wi-Fi interface is active
-        if self.wifi.active():
-            # Deactivate the Wi-Fi interface
-           self.wifi.active(False)
-
-        # Check if the device is not connected to any Wi-Fi network
-        if not self.wifi.isconnected():
-            print("Disconnected")
-
-   
 class WeatherDisplay:
-    def __init__(self,wifi_manager):
-        """
-        Initializes the Weather Station object with default and configurable parameters.
+    def __init__(self,wifiManager,displayManager):
 
-        This method sets up the Weather Station object with various configuration parameters, including PIN assignments for sensors, Wi-Fi credentials, OpenWeatherMap API key, city for weather data, and units for temperature measurement. It also initializes the I2C interface for the LCD, BME280 sensor for temperature, humidity, and pressure, and ADC (Analog-to-Digital Converter) for light intensity readings. The method creates instances of the WaterSensor and LDR classes for soil moisture and light intensity sensing, respectively.
-
-        Attributes:
-            LDR_PIN (int): The GPIO pin for the LDR (Light Dependent Resistor) sensor.
-            WATER_SENSOR_PIN (int): The GPIO pin for the soil moisture sensor.
-            ssid (str): The Wi-Fi SSID for network connection.
-            password (str): The Wi-Fi password for network connection.
-            api_key (str): The API key for accessing the OpenWeatherMap API.
-            city (str): The default city for weather data (BRNO, Czech Republic in this case).
-            exclude (str): Parameter for excluding specific weather data from the OpenWeatherMap API response.
-            units (str): The unit system for temperature measurement (metric in this case).
-            i2c (I2C): The I2C interface for communication with devices like the LCD.
-            i2c_addr (int): The I2C address of the LCD.
-            lcd (I2cLcd): An instance of the I2cLcd class for interfacing with the LCD.
-            bme (BME280): An instance of the BME280 class for temperature, humidity, and pressure sensing.
-            ldr_adc (ADC): An instance of the Analog-to-Digital Converter for reading light intensity values.
-            water_sensor (WaterSensor): An instance of the WaterSensor class for soil moisture sensing.
-            ldr (LDR): An instance of the LDR class for light intensity sensing.
-            wifi (network.WLAN): An instance of the WLAN class for Wi-Fi connectivity.
-        """
         self.LDR_PIN = 15
         self.WATER_SENSOR_PIN = 36
         self.api_key = "ce0138a17e760fac657ac45c93b0fa9b"
         self.city = "BRNO,cz"  # todo further implementation geolocation api
-        self.exclude = ""
+        self.exclude="current,minutely,hourly,alerts"
         self.units = "metric"
-
-        self.i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=100_000)
-        self.i2c_addr = 0x27
-        self.lcd = I2cLcd(self.i2c, self.i2c_addr, num_lines=2, num_columns=16)
-
+        
+        self.i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=400_000)
         self.bme = bme280.BME280(i2c=self.i2c, addr=0x76)
-        self.ldr_adc = ADC(Pin(self.LDR_PIN))
         self.water_sensor = WaterSensor(self.WATER_SENSOR_PIN)
         self.ldr = LDR(self.LDR_PIN)
 
-        self.wifi_manager = wifi_manager
-
-
-
+        self.wifiManager = wifiManager
+        self.displayManager= displayManager
+        
     def read_sensors(self):
         """
         Reads data from various sensors, including a soil moisture sensor, a light sensor, and a BME sensor (temperature, humidity, and pressure).
@@ -129,111 +52,69 @@ class WeatherDisplay:
         return moisture, light_intensity, temp, hum, pres
 
     def display_sensor_values(self, moisture, light_intensity, temp, hum, pres):
-        """
-        Displays sensor readings on a connected LCD screen in a formatted layout.
 
-        This method takes sensor readings as input and presents them on a connected LCD screen. The displayed information includes soil moisture level, light intensity, temperature, humidity, and atmospheric pressure. The data is arranged in a clear and organized format to provide easy visibility and interpretation.
-
-        Parameters:
-            moisture (float): The soil moisture level, represented as a percentage.
-            light_intensity (float): The intensity of light, represented as a percentage. Note: Placeholder value until an actual light sensor is implemented.
-            temp (float): The temperature reading from the BME sensor.
-            hum (float): The humidity reading from the BME sensor.
-            pres (float): The atmospheric pressure reading from the BME sensor.
-        """
-        self.lcd.clear()
-        self.lcd.double_print("Moisture", "{:.1f}%".format(moisture), "Light", "{:.1f}%".format(light_intensity))
-        self.lcd.double_print("Temp", temp, "Humidity", hum)
-        self.lcd.single_print("Pressure", pres)
+        self.displayManager.lcd.clear()
+        self.displayManager.double_print("Moisture", "{:.1f}%".format(moisture), "Light", "{:.1f}%".format(light_intensity))
+        self.displayManager.double_print("Temp", temp, "Humidity", hum)
+        self.displayManager.single_print("Pressure", pres)
 
     def fetch_weather_data(self):
-        """
-        Fetches current weather data from the OpenWeatherMap API based on specified parameters.
 
-        This method constructs a URL for the OpenWeatherMap API using the provided city, exclusion list, units, and API key.
-        It then makes a request to the API, retrieves the weather data in JSON format, and closes the connection.
-        After fetching the data, the function disconnects from the Wi-Fi network.
-
-        Returns:
-            dict: A dictionary containing the current weather information, including temperature, humidity, wind speed, and more, as provided by the OpenWeatherMap API.
-        """
+        self.wifiManager.connect_wifi()
+        
         url_call = (
-            "https://api.openweathermap.org/data/2.5/weather?"
+            "https://api.openweathermap.org/data/2.5/forecast?"
             f"q={self.city}&exclude={self.exclude}&units={self.units}&appid={self.api_key}"
         )
+        
         print(url_call)  # Print the API request URL for debugging purposes
         w = urequests.get(url_call)
         weather_data = json.loads(w.content)
         w.close()
-        self.wifi_manager.disconnect_wifi()
+        self.wifiManager.disconnect_wifi()
 
         return weather_data
 
     def display_weather(self):
-            
-        """
-        Displays current weather information on a connected LCD screen, including details such as temperature, humidity, wind speed, and more.
+                 
+        forecast = self.fetch_weather_data()
+        
+        print(forecast.keys())
+        for day_data in forecast['list']:
+            # Extract the hour from the dt_txt field
+            hour = int(day_data['dt_txt'].split(' ')[1].split(':')[0])
 
-        This method fetches weather data from the OpenWeatherMap API using the `fetch_weather_data` function and extracts relevant information
-        to display on a connected LCD screen. The displayed information includes the city name, current weather conditions, temperature range,
-        and additional details like wind speed, humidity, and sunrise/sunset times. The function also integrates sensor readings for soil moisture,
-        light intensity, temperature, humidity, and pressure.
+            # Check if the hour is 12 (noon)
+            if hour == 0 or hour==12:
+                temp = int(day_data['main']['temp'])
 
-        """
-        self.wifi_manager.connect_wifi()
-        weather_data = self.fetch_weather_data()
+                if 'rain' in day_data :
+                    rain_qty = day_data['rain']
+                else:
+                    rain_qty = 0
 
-        # Extracting information from the weather_data JSON response
-        coord = weather_data.get('coord', {})
-        lon = coord.get('lon', '')
-        lat = coord.get('lat', '')
+                weather_main = day_data['weather'][0]['main']
+                weather_descr = day_data['weather'][0]['description']
+                self.displayManager.lcd.clear()
+                self.displayManager.lcd.move_to(0,0)
+                self.displayManager.lcd.print(day_data['dt_txt'])
+                self.displayManager.lcd.move_to(0,1)
+                self.displayManager.lcd.print("Weather: "+weather_main)
+                
+                self.displayManager.oled.fill(0)
+                self.displayManager.oled_print("Temperature:"+str(temp)+" C",0,0)
+                print("Temperature: {}°C".format(temp))
+                print("Rain: {} mm ".format(rain_qty))
+                print("-" * 20)
+                sleep(2)
 
-        weather = weather_data.get('weather', [{}])[0]
-        weather_id = weather.get('id', '')
-        weather_main = weather.get('main', '')
-        weather_description = weather.get('description', '')
-        weather_icon = weather.get('icon', '')
-
-        main_info = weather_data.get('main', {})
-        temp = str(main_info.get('temp', ''))
-        feels_like = str(main_info.get('feels_like', ''))
-        temp_min = str(main_info.get('temp_min', ''))
-        temp_max = str(main_info.get('temp_max', ''))
-        pressure = str(main_info.get('pressure', ''))
-        humidity = str(main_info.get('humidity', ''))
-
-        wind_info = weather_data.get('wind', {})
-        wind_speed = str(wind_info.get('speed', ''))
-        wind_deg = str(wind_info.get('deg', ''))
-
-        clouds_info = weather_data.get('clouds', {})
-        cloudiness = str(clouds_info.get('all', ''))
-
-        sys_info = weather_data.get('sys', {})
-        sys_type = str(sys_info.get('type', ''))
-        sys_id = str(sys_info.get('id', ''))
-        sys_country = sys_info.get('country', '')
-        sys_sunrise = str(sys_info.get('sunrise', ''))
-        sys_sunset = str(sys_info.get('sunset', ''))
-
-        visibility = str(weather_data.get('visibility', ''))
-        dt = str(weather_data.get('dt', ''))
-        timezone = str(weather_data.get('timezone', ''))
-        city_id = str(weather_data.get('id', ''))
-        city_name = weather_data.get('name', '')
-        cod = str(weather_data.get('cod', ''))
-
-        self.lcd.single_print("City", city_name)
-
-        # Additional prints can be added here for more detailed weather information
-        # print(weather_main)
-        # print(temp_min, temp_max)
-        # print(temp)
+        # Add sleep if needed
+        sleep(1)
 
         moisture, light_intensity, temp, hum, pres = self.read_sensors()
         self.display_sensor_values(moisture, light_intensity, temp, hum, pres)
 
-        self.lcd.clear()
+        self.displayManager.lcd.clear()
 
 
 
@@ -262,9 +143,12 @@ class WeatherDisplay:
     
 
 if __name__ == "__main__":
-    wifi_manager = WiFiManager(ssid="rrrr", password="12345678")
-    weather_display = WeatherDisplay(wifi_manager)
+    wifiManager = WiFiManager(ssid="rrrr", password="12345678")
+    displayManager = DisplayManager()
+    weather_display = WeatherDisplay(wifiManager,displayManager)
 
     while True: 
             weather_display.display_weather()
-            lightsleep(10*60*1000)
+            print("Going to sleep")
+            sleep(90)
+            #lightsleep(10*60*100)
---
+++ src/utilities/display_manager.py
@@ -0,0 +1,80 @@
+from machine import Pin, I2C
+from lcd_api import LcdApi
+from i2c_lcd import I2cLcd
+from lcd_oled import SH1106_I2C
+from machine import soft_reset
+
+
+class DisplayManager():
+    
+    def __init__(self):
+               
+        self.WIDTH = 128  # OLED display width
+        self.HEIGHT = 64  # OLED display height
+
+        # Init I2C using pins GP22 & GP21 (default I2C0 pins)
+        self.i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=400_000)
+        print(f"I2C address       : {hex(self.i2c.scan()[1])}")
+        print(f"I2C configuration : {str(self.i2c)}")
+
+        print(self.i2c)
+        self.oled = SH1106_I2C(self.WIDTH, self.HEIGHT, self.i2c, addr=0x3C, rotate=180)
+
+        self.oled.text("Using OLED and a a", x=0, y=40)
+        self.oled.text("ESP32", x=50, y=50)
+        self.oled.show()
+
+        self.i2c_addr = 0x27
+        self.lcd = I2cLcd(self.i2c, self.i2c_addr, num_lines=2, num_columns=16)
+     
+        self.lcd.display_on()
+        
+    def double_print(self, first_sensor, first_value, second_sensor,second_value):
+        self.lcd.backlight_on()
+        self.lcd.clear()
+        self.lcd.move_to(0,0)
+        self.lcd.print(first_sensor)
+
+        self.lcd.move_to(8,0)
+        self.lcd.print(":")
+        
+        if first_value[1]=='.':
+            self.lcd.move_to(11,0)
+        else:
+            self.lcd.move_to(10,0)        
+        self.lcd.print(first_value)
+       
+        self.lcd.move_to(0,1)
+        self.lcd.print(second_sensor)
+        
+        self.lcd.move_to(8,1)
+        self.lcd.print(":")
+
+        if second_value[1]=='.':
+            self.lcd.move_to(11,1)
+        else:
+            self.lcd.move_to(10,1)        
+            self.lcd.print(second_value)
+            
+        time.sleep(1)
+        self.lcd.backlight_off()
+        
+        
+    def single_print(self,first_sensor, first_value):
+        self.lcd.backlight_on()
+        self.lcd.clear()
+        self.lcd.move_to(0,0)
+        self.lcd.print(first_sensor)
+        
+        self.lcd.move_to(8,0)
+        self.lcd.print(":")
+        
+        self.lcd.move_to(7,1)        
+        self.lcd.print(first_value)
+       
+        time.sleep(1)
+        self.lcd.backlight_off()
+        
+        
+
+        
