f68b1b1c8f441c3cc11c588323f5ca6f5916658c
raszvan <r_cacu@yahoo.com>
2023-12-06 20:39:41 +0100

    finishing touches

--- utilities/display_manager.py
+++ utilities/display_manager.py
@@ -1,7 +1,6 @@
 from machine import Pin, I2C
-from lcd_api import LcdApi
 from i2c_lcd import I2cLcd
-from lcd_oled import SH1106_I2C
+from oled_display import SH1106_I2C
 
 
 class DisplayManager():
@@ -17,15 +16,8 @@ class DisplayManager():
 
         # Init I2C using pins GP22 & GP21 (default I2C0 pins)
         self.i2c = I2C(0, scl=Pin(22), sda=Pin(21), freq=400_000)
-        print(f"I2C address       : {hex(self.i2c.scan()[1])}")
-        print(f"I2C configuration : {str(self.i2c)}")
-
-        print(self.i2c)
-        self.oled = SH1106_I2C(self.WIDTH, self.HEIGHT, self.i2c, addr=0x3C, rotate=180)
-
-        self.oled.text("Using OLED and a a", x=0, y=40)
-        self.oled.text("ESP32", x=50, y=50)
-        self.oled.show()
+        
+        #self.oled = SH1106_I2C(self.WIDTH, self.HEIGHT, self.i2c, addr=0x3C, rotate=180)
 
         self.i2c_addr = 0x27
         self.lcd = I2cLcd(self.i2c, self.i2c_addr, num_lines=2, num_columns=16)
@@ -44,7 +36,6 @@ class DisplayManager():
         - second_value (str): Value of the second sensor.
         """
         
-        self.lcd.backlight_on()
         self.lcd.clear()
         self.lcd.move_to(0,0)
         self.lcd.print(first_sensor)
@@ -71,7 +62,7 @@ class DisplayManager():
             self.lcd.print(second_value)
             
         time.sleep(1)
-        self.lcd.backlight_off()
+    
         
         
     def single_print(self,first_sensor, first_value):
@@ -83,8 +74,7 @@ class DisplayManager():
         - first_sensor (str): Name of the sensor.
         - first_value (str): Value of the sensor.
         """
-         
-        self.lcd.backlight_on()
+        
         self.lcd.clear()
         self.lcd.move_to(0,0)
         self.lcd.print(first_sensor)
@@ -96,7 +86,6 @@ class DisplayManager():
         self.lcd.print(first_value)
        
         time.sleep(1)
-        self.lcd.backlight_off()
         
         
 
